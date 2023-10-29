4c6d6a34aacb3c3303d091c71328d968694028bd
LoganH-R <146144771+LoganH-R@users.noreply.github.com>
2023-10-29 03:39:04 +0000

    almost all

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,61 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def directors_list
+    @directors = Director.all
+
+    render({ :template => "misc_templates/directors_all" })
+  end
+
+  def find_youngest_director
+    @directors = Director.all.order(:dob => :asc)
+    @youngest_director = @directors.last
+    @youngest_director_dob = @youngest_director.dob.strftime("%B %e, %Y")
+
+    render({ :template => "misc_templates/director_youngest" })
+  end
+
+  def find_eldest_director
+    @directors = Director.all.order(:dob => :asc)
+    @eldest_director = @directors.first
+    @eldest_director_dob = @eldest_director.dob.strftime("%B %e, %Y")
+
+    render({ :template => "misc_templates/director_eldest" })
+  end
+
+  def find_director
+    id_of_director = params.fetch("director_id")
+    director_activerecordrelation = Director.where({ :id => id_of_director })
+    @individual_director = director_activerecordrelation.first
+
+    @director_filmography = Movie.where({ :director_id => id_of_director })
+
+    render({ :template => "misc_templates/director_individual_flexible" })
+  end
+
+  def movies_list
+    @movies = Movie.all
+    @directors = Director.all
+
+    render({ :template => "misc_templates/movies_all" })
+  end
+
+  def find_movie
+
+
+    render({ :template => "misc_templates/movie_individual_flexible" })
+  end
+
+  def actors_list
+    @actors = Actor.all
+
+    render({ :template => "misc_templates/actors_all" })
+  end
+
+  def find_actor
+
+
+    render({ :template => "misc_templates/actor_individual_flexible" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors", { :controller => "misc", :action => "directors_list" })
+  get("/directors/youngest", { :controller => "misc", :action => "find_youngest_director" })
+  get("/directors/eldest", { :controller => "misc", :action => "find_eldest_director" })
+  get("/directors/:director_id", { :controller => "misc", :action => "find_director" })
+
+  get("/movies", { :controller => "misc", :action => "movies_list" })
+  get("/movies/:movie_id", { :controller => "misc", :action => "find_movie" })
+
+  get("/actors", { :controller => "misc", :action => "actors_list" })
+  get("/actors/:actor_id", { :controller => "misc", :action => "find_actor" })
 end
